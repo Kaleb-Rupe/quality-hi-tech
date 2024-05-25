f754eb4a444cfe756ac430e11881a5eac5c00bb6
Zack Honey <zhoney466@gmail.com>
2024-05-24 18:31:08 -0700

    Moved images into src folder

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import logo from '../../images/logo.JPG'
 
 const Header = () => {
     return (
         <>
             <header>
+                <img src={logo} alt='Logo'/>
                 <h1>
                     Santa Cruz Sun Construction
                 </h1>
