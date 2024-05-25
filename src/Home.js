ec3db6edb57acb5b7deda10fbc8be1a6a3120729
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2024-05-25 16:03:45 -0400

    Resolved merge issue.

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
 import Footer from "./Footer/Footer.js";
+import Header from "./Header/Header.js";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Footer />
     </>
   );
