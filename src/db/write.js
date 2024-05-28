fd2c891689768232633a00619af2b362a4459c27
Zack Honey <zhoney466@gmail.com>
2024-05-28 13:56:09 -0700

    possibly got database worked out

--- src/db/write.js
+++
@@ -1,7 +0,0 @@
-const {writeDb} = require('./dbFunctions');
-
-const obj = {
-    img: 'IMG_0162.jpeg'
-}
-
-writeDb(obj)
---
+++ write.js
@@ -0,0 +1,9 @@
+const {writeDb} = require('./dbFunctions');
+
+const obj = [
+    
+        {"img": 'IMG_0162.jpeg'}
+    
+]
+
+writeDb(obj)
