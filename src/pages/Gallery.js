75e6d02bd3f37e14d8647a60bd2e6ca9ffda77a7
Zack Honey <zhoney466@gmail.com>
2024-05-28 11:22:51 -0700

    scrapped database I tried to make. It broke the app. Gonna research more and mess with it

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,19 +1,14 @@
 import React from "react";
-import assets from '../assets'
 
 
-const Image = () => {
-  for(let i = 0; i < assets.length; i++){
-    return i;
-  }
-}; 
+
 
 const Gallery = () => {
   return (
     <>
       <h1>Gallery Page</h1>
       <p>Welcome to the gallery page!</p>
-      <img src={Image} />
+     
     </>
   );
 };
