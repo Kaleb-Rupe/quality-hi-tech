8949d1d66281ecde7296a8650d0076dba0eee482
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2024-05-28 03:21:45 -0400

    Added header css, react router, nav links, and improved UI/UX as well as added a login button.

--- src/components/Footer-Comp/footer_nav_socials.js
+++ src/components/Footer-Comp/footer_nav_socials.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Link } from "react-router-dom";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import "./footer_nav_socials.css";
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import "./Footer.css";
 import Form from "./Footer-Comp/form_page";
 import Details from "./Footer-Comp/details_page";
