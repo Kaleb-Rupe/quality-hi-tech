b6bae05d472d54ac1666be7acffdb7fa5e5a1971
Zachary Honey <zhoney466@gmail.com>
2024-06-17 09:44:06 -0700

    Added services info with client

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -18,6 +18,42 @@ export const Services = () => {
           <h3>Project Management</h3>
           <p>End-to-end project management for a seamless experience.</p>
         </div>
+        <div className="service-card">
+          <h3>Kitchen Remodels</h3>
+          <p>Update your kitchen with a custom hood and new cabinets!</p>
+        </div>
+        <div className="service-card">
+          <h3>Bathroom Remodels</h3>
+          <p>Transform your old bathroom into a spa!</p>
+        </div>
+        <div className="service-card">
+          <h3>Basement Finishing</h3>
+          <p>Got an empty basement? Let us tranform that into a family space!</p>
+        </div>
+        <div className="service-card">
+          <h3>Decks</h3>
+          <p>BBQ season is here! Let us build an outdoor haven for your family!</p>
+        </div>
+        <div className="service-card">
+          <h3>Sheds</h3>
+          <p>Do you need extra storage or work space? Let us provide options!</p>
+        </div>
+        <div className="service-card">
+          <h3>Flooring</h3>
+          <p>New floors will transform any space!</p>
+        </div>
+        <div className="service-card">
+          <h3>Custom Trimwork</h3>
+          <p>Custom trim can add class and elegance to any home!</p>
+        </div>
+        <div className="service-card">
+          <h3>Home Additions</h3>
+          <p>Looking for a get away? We can build one for you!</p>
+        </div>
+        <div className="service-card">
+          <h3>She-Sheds/Mancaves</h3>
+          <p>Let us build a private retreat!</p>
+        </div>
       </div>
     </section>
   );
