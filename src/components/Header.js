e8b615e3e7253c623a38cc8e77b9e9a4331eefa8
MightyMags <cure.pet.blond@cloak.id>
2024-06-21 13:21:54 -0400

    Fixed the header for mobile and table.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,61 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.JPG";
 import "./Header.css";
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <header id="masthead" className="site-header">
-        <div className="header-logo">
-        <a href="/" id="logo" onclick="document.location=/home'.html';return false;" >
-          <img src={Logo} alt="logo" />
-        </a>
-          <h1>Santa Cruz Sun LLC, CCB#249418</h1>
+        <div className="header-content">
+          <div className="header-logo">
+            <a href="/" id="logo" onClick={(e) => e.preventDefault()}>
+              <img src={Logo} alt="logo" />
+            </a>
+            <h1 className="site-title">Santa Cruz Sun LLC, CCB#249418</h1>
+          </div>
+          <div className="menu-toggle" onClick={toggleMenu}>
+            <div className={`hamburger ${menuOpen ? "open" : ""}`}></div>
+            <div className={`hamburger ${menuOpen ? "open" : ""}`}></div>
+            <div className={`hamburger ${menuOpen ? "open" : ""}`}></div>
+          </div>
         </div>
-        <nav className="header-nav">
+        <nav className={`header-nav ${menuOpen ? "open" : ""}`}>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={() => setMenuOpen(false)}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/services">Services</Link>
+              <Link to="/services" onClick={() => setMenuOpen(false)}>
+                Services
+              </Link>
             </li>
             <li>
-              <Link to="/gallery">Gallery</Link>
+              <Link to="/gallery" onClick={() => setMenuOpen(false)}>
+                Gallery
+              </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={() => setMenuOpen(false)}>
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" onClick={() => setMenuOpen(false)}>
+                Contact
+              </Link>
             </li>
-            {/* <li className="login-button">
-              <Link to="/login">Login</Link>
-            </li> */}
           </ul>
         </nav>
       </header>
     </>
   );
 };
-
