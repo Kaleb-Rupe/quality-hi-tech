27056717a2233734ed22a554d3ecacf99f4125d2
MightyMags <cure.pet.blond@cloak.id>
2024-06-22 19:59:54 -0400

    Fixed the images for mobile

--- src/components/Home/Featured-Gallery/featured-gallery.js
+++ src/components/Home/Featured-Gallery/featured-gallery.js
@@ -1,16 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Modal from "react-modal";
 import "../../../css/featured-gallery.css";
 
 import { images } from "./gallery-data";
 
-Modal.setAppElement("#root"); // This is important for accessibility reasons
+Modal.setAppElement("#root");
 
-const FeaturedGallery = () => {
+const ArrowLeft = () => (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+  </svg>
+);
+
+const ArrowRight = () => (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+  </svg>
+);
+
+const FeaturedGallery = React.memo(() => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(null);
   const [visibleStart, setVisibleStart] = useState(0);
-  const visibleCount = 4; // Number of images visible at a time
+  const visibleCount = 4;
+  const galleryRef = useRef(null);
+
+  useEffect(() => {
+    const gallery = galleryRef.current;
+    let startX, startY, distX, distY;
+    let isSwiping = false;
+
+    const handleTouchStart = (e) => {
+      startX = e.touches[0].clientX;
+      startY = e.touches[0].clientY;
+      isSwiping = true;
+    };
+
+    const handleTouchMove = (e) => {
+      if (!isSwiping) return;
+      distX = e.touches[0].clientX - startX;
+      distY = e.touches[0].clientY - startY;
+    };
+
+    const handleTouchEnd = () => {
+      if (!isSwiping) return;
+      if (Math.abs(distX) > Math.abs(distY)) {
+        if (distX > 50 && visibleStart > 0) {
+          prevImages();
+        } else if (distX < -50 && visibleStart + visibleCount < images.length) {
+          nextImages();
+        }
+      }
+      isSwiping = false;
+    };
+
+    gallery.addEventListener("touchstart", handleTouchStart);
+    gallery.addEventListener("touchmove", handleTouchMove);
+    gallery.addEventListener("touchend", handleTouchEnd);
+
+    return () => {
+      gallery.removeEventListener("touchstart", handleTouchStart);
+      gallery.removeEventListener("touchmove", handleTouchMove);
+      gallery.removeEventListener("touchend", handleTouchEnd);
+    };
+  }, [visibleStart]);
 
   const openModal = (image) => {
     setCurrentImage(image);
@@ -33,12 +86,15 @@ const FeaturedGallery = () => {
   };
 
   return (
-    <div className="gallery-container">
-      {visibleStart > 0 && ( // Show left arrow if not at the beginning
-        <button className="nav-button left" onClick={prevImages}>
-          &#10094;
-        </button>
-      )}
+    <div className="gallery-container" ref={galleryRef}>
+      <button
+        className="nav-button left"
+        onClick={prevImages}
+        disabled={visibleStart === 0}
+        aria-label="Previous images"
+      >
+        <ArrowLeft />
+      </button>
       <div className="gallery">
         {images
           .slice(visibleStart, visibleStart + visibleCount)
@@ -47,16 +103,23 @@ const FeaturedGallery = () => {
               key={index}
               className="gallery-item"
               onClick={() => openModal(image)}
+              tabIndex={0}
+              role="button"
+              aria-label={`View larger image of ${image.alt}`}
+              onKeyDown={(e) => e.key === "Enter" && openModal(image)}
             >
               <img src={image.src} alt={image.alt} loading="lazy" />
             </div>
           ))}
       </div>
-      {visibleStart + visibleCount < images.length && ( // Show right arrow if more images available
-        <button className="nav-button right" onClick={nextImages}>
-          &#10095;
-        </button>
-      )}
+      <button
+        className="nav-button right"
+        onClick={nextImages}
+        disabled={visibleStart + visibleCount >= images.length}
+        aria-label="Next images"
+      >
+        <ArrowRight />
+      </button>
 
       <Modal
         isOpen={modalIsOpen}
@@ -66,7 +129,11 @@ const FeaturedGallery = () => {
       >
         {currentImage && (
           <div>
-            <button className="close-button" onClick={closeModal}>
+            <button
+              className="close-button"
+              onClick={closeModal}
+              aria-label="Close modal"
+            >
               &times;
             </button>
             <img
@@ -79,6 +146,6 @@ const FeaturedGallery = () => {
       </Modal>
     </div>
   );
-};
+});
 
 export default FeaturedGallery;
