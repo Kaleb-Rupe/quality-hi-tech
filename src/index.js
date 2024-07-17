c16d46034c16c3183a29cb3bdad636cf82e55459
MightyMags <cure.pet.blond@cloak.id>
2024-07-17 04:19:11 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -24,6 +24,19 @@ if (!rootElement) {
 } else {
   Modal.setAppElement(rootElement);
 
+  function adjustViewportHeight() {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+
+  // We listen to the resize event
+  window.addEventListener("resize", adjustViewportHeight);
+
+  // We call the function for the first time to set the initial value
+  adjustViewportHeight();
+
   const root = createRoot(rootElement);
 
   root.render(
